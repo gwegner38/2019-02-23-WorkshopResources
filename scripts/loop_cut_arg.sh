a705a1fdee5f4499c38b8316f882704037060d68
Graham Wegner <gwegner38@gmail.com>
2019-02-23 16:32:45 -0700

    day one

---
+++ scripts/loop_cut_arg.sh
@@ -0,0 +1,8 @@
+echo "columns for cut $1 $2 $3"
+echo "number of lines for head $4"
+echo "sort options $5"
+for gapminderfile in [U-Z]*.txt
+do
+    echo $gapminderfile
+    cut -f "$1","$2","3" $gapminderfile | sort -nr | head -"$4"
+done
